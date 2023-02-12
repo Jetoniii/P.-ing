ac6e43c2028ba0be19cac49beec97a7e06fe2c65
Jetoniii <jetonkameri0@gmail.com>
2023-02-12 13:50:23 +0100

    i

--- Users.php
+++ Users.php
@@ -8,9 +8,7 @@ $DATABASE='signupforms';
 
 $con=mysqli_connect($HOSTNAME,$USERNAME,$PASSWORD,$DATABASE);
 
-if($con){
-  echo "Connection successful";
-}else{
+if(!$con){
   die(mysqli_error($con));
 }
 
--- loginpage.php
+++ loginpage.php
@@ -1,18 +1,31 @@
 <?php
+$succes=0;
+$user=0;
 
 if($_SERVER['REQUEST_METHOD']=='POST'){
     include 'Users.php';
     $username = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
+ 
 
-    $sql="insert into 'registration' (username,email,password)values('$username','$email','$password')";
+
+    $sql="Select * from `registration`where username='$username'";
+
     $result = mysqli_query($con,$sql);
-
     if($result){
-        echo "Data inserted succesfully";
-    }else{
-        die(mysqli_error($con));
+        $num=mysqli_num_rows($result);
+        if($num>0){
+            $user=1;
+        }else{
+            $sql="insert into registration (username,email,password) values ('$username','$password','$email')";
+            $result = mysqli_query($con,$sql);
+            if($result){
+            $succes=1;
+            }else{
+               die(mysqli_error($con));
+    }
+        }
     }
 }
 
@@ -23,6 +36,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="styles/loginstyle.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -30,10 +44,78 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
     <link rel="preconnect" href="https:/fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="loginstyle.css">
     <script defer src="loginscript.js"></script>
 </head>
 <body>
+
+
+<?php
+if ($user) {
+    echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <strong>Error:</strong> User already exists. Please choose a different username.
+       
+    </div>';
+    echo '<style>
+        .alert {
+            margin-top: 0px;
+            position: relative;
+            background-color: #f8d7da;
+            border-color: #f5c6cb;
+            color: #721c24;
+            padding: .75rem 1.25rem;
+            border-radius: .25rem;
+        }
+        
+        .alert strong {
+            font-weight: bold;
+        }
+        
+        .alert button {
+            position: absolute;
+            top: 50%;
+            right: 0;
+            transform: translateY(-50%);
+        }
+    </style>';
+}
+
+
+?>
+
+<?php
+if ($succes) {
+    echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Success:</strong> Registration complete. Thank you for signing up!
+    </div>';
+    echo '<style>
+        .alert {
+            margin-top: 0px;
+            position: relative;
+            background-color: #d4edda;
+            border-color: #c3e6cb;
+            color: #155724;
+            padding: .75rem 1.25rem;
+            border-radius: .25rem;
+        }
+        
+        .alert strong {
+            font-weight: bold;
+        }
+        
+        .alert button {
+            position: absolute;
+            top: 50%;
+            right: 0;
+            transform: translateY(-50%);
+        }
+    </style>';
+}
+
+
+
+
+?>
+
     <div class="container">
         <form id="form" action="loginpage.php" method="post" onsubmit="return validateForm()">
             <h1>Registration</h1>
@@ -58,6 +140,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
                 <div class="error"></div>
             </div>
             <button type="submit">Sign Up</button>
+            <p>Already have an Account <a href="qwertyu.html" >Click Here</p>
         </form>
     </div>
     
